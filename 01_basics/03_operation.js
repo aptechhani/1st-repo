8d9b769d366600b5a8e26a9511a63282e6b6f396
aptechhani <aptechhani@gmail.com>
2024-09-03 21:36:19 +0000

    operation configure

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,54 @@
+
+//  let someNumber =89
+
+//  let stringNumber  = string(someNumber)
+// // console.log(stringNumber);
+
+// //operations 
+
+//  let value =3 
+//   let negvalue = -value 
+// //  console.log(negvalue)
+
+// let str1 = "hani"
+// let str2 = "sarfraz"
+// let str3 = str1 + str2
+// // console.log(str3)
+
+
+// console.log("1"+ 2 );
+// console.log(1+ "2" );
+// console.log("1" +"2")
+// console.log(1+1 +"2")
+// console.log ("1" +1+2)
+// console.log (1+"2"+1)
+// console.log(true)
+// console.log (+true)
+
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2+2
+// console.log (num3)
+
+
+let gamecounter = 200
+gamecounter++;
+console.log(gamecounter)
+
+let gamescounter = 200
+++gamescounter;
+console.log(gamescounter)
+
+
+
+
+let gaamecounter = 200
+gaamecounter--;
+console.log(gaamecounter)
+
+let gamescounterr = 200
+--gamescounterr;
+console.log(gamescounterr)
+
+
+
