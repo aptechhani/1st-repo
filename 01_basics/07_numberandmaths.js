5d3c88e1c5098384e086251c1c55aa8fa3d6e5ff
aptechhani <aptechhani@gmail.com>
2024-09-11 20:26:58 +0000

    mathandnum configure

---
+++ 01_basics/07_numberandmaths.js
@@ -0,0 +1,33 @@
+const scoree = 1000
+console.log(scoree);
+
+
+const scores = new Number (2000)
+console.log(typeof(2000))
+
+console.log(scores.toString());
+console.log(scores.toFixed(4));
+
+
+
+const number = 5787.80
+console.log(number.toPrecision(4));
+
+
+const hundred = 100000000
+console.log(hundred.toLocaleString(100000000))
+console.log(hundred.toLocaleString('en-PAKISTAN'))
+console.log(hundred.toLocaleString('en-IN'))
+
+
+console.log(Math);
+console.log(Math.abs(-34))
+console.log(Math.round(1803.9))
+console.log(Math.sqrt(4))
+console.log(Math.random())
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1))+ min)
+
