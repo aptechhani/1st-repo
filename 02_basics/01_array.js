8b9339b38804980d30c2509c6250fb6c3f866c2a
aptechhani <aptechhani@gmail.com>
2024-11-25 09:12:27 +0000

    array configure

---
+++ 02_basics/01_array.js
@@ -0,0 +1,67 @@
+//Array
+
+
+ const myArr = [1,2,3,4,5];
+ const myNames= ["haya" , "khadija"];
+//  console.log(myArr);
+
+
+// array Method 
+//push 
+// myArr.push(34);
+// console.log(myArr);
+
+// // pop
+// myNames.pop();
+// console.log(myNames);
+
+
+//unshift
+
+const myCar= ["lamborgini" , "ferrrari" , "porche"];
+myCar.unshift("BMW");
+// console.log(myCar);
+
+
+// shift
+const myCars= ["lamborgini" , "ferrrari" , "porche"];
+myCars.shift();
+// console.log(myCars);
+
+// includes
+
+const myCarr = ["lamborgini" , "ferrrari" , "porche"];
+
+// console.log(myCarr.includes("suzuki"));
+
+
+//indexOf
+
+
+const myCarrr = ["lamborgini" , "ferrari" , "porche"];
+
+// console.log(myCarrr.indexOf("ferrari"));
+
+
+// join 
+
+const newArr = myCarrr.join()
+// console.log(typeof newArr);
+// console.log(myCarrr);
+
+
+
+// slice or splice
+
+// samjh nhi arha 
+
+const numb = [1,2,3,4,5]
+
+const Arr = numb.slice(0,2);
+console.log(Arr);
+
+//splice
+const numbe = [1,2,3,4,5]
+
+const Arre = numbe.splice(1,3);
+console.log(Arre);
