f3cedbf3917cd86219c5cb086652ce64fc00b2ac
aptechhani <aptechhani@gmail.com>
2025-01-28 14:52:07 +0000

    callback configure

---
+++ 03_basics/05_callback.js
@@ -0,0 +1,16 @@
+// Function to start cooking
+function start(cooking) {
+    console.log("Cooking " + cooking);  // Cooking ke saath cooking ka text print hoga
+  }
+  
+  // Function to finish cooking
+  function finish(cooking) {
+    console.log("Cooking " + cooking);  // Finish ke saath cooking ka text print hoga
+  }
+  
+  // Calling start function
+  start("started");  // Output: "Cooking started"
+  
+  // Calling finish function
+  finish("finished");  // Output: "Cooking finished"
+  
