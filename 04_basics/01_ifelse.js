241ec3a1e1702cd2e241b31aa206c9c219a64b01
aptechhani <aptechhani@gmail.com>
2025-01-29 11:17:32 +0000

    ifelse configure

---
+++ 04_basics/01_ifelse.js
@@ -0,0 +1,21 @@
+//If the number is greater than zero, print "Positive".
+// If the number is less than zero, print "Negative".
+// If the numb
+// er is equal to zero, print "Zero".
+const number=;
+
+if (number>0) {
+    console.log("positive");
+}
+else if(number<0){
+    console.log("negative");
+}
+    else if(number==0)
+    {
+    console.log("zero")
+    }
+
+// else
+// {
+//     console.log("this number is not correct")
+// }
