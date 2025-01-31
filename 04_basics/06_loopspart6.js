52a9549dcf3e363afe94e4af6c6868a3f9b5b5cf
aptechhani <aptechhani@gmail.com>
2025-01-31 20:04:09 +0000

    loops5 configure

---
+++ 04_basics/06_loopspart6.js
@@ -0,0 +1,18 @@
+
+// fpr each looop
+
+const coding =['js' , 'ruby' , 'html','css' , 'python']
+
+
+coding.forEach(function(val) {
+    console.log(val)
+})
+
+// another declare with arroaw 
+
+const codings =['js' , 'ruby' , 'html','css' , 'python']
+
+
+codings.forEach((vasl) => {
+    console.log(vasl)
+})
