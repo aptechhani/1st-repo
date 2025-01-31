fd346364ecc80a98b9ba64641e0fe997b20d2c1b
aptechhani <aptechhani@gmail.com>
2025-01-31 20:03:47 +0000

    loop3 configure

---
+++ 04_basics/05_loopspart5.js
@@ -0,0 +1,45 @@
+
+
+ /// loops for object  keys k lie ye ${key}  or vlaues k lie  ${myobj[key]}
+const myobj = {
+     js : "javacsript" ,
+     cpp : "C++" ,
+     rb : "ruby",
+     css : "cascading  style sheet",
+
+} 
+for (const key in myobj) {
+    // console.log(`${key} shortcut these languages is ${myobj[key]}`) ;
+}
+
+
+// give forin loop in array => to agr hm for in loop m array ko rpint krengy to ans me hmray pass counting
+// aye gi arry print nhi hongy jese k ye 
+
+const programming = [ 'css' , 'js' , 'html' , 'rb' , 'cpp']
+for (const keys in programming) {
+//    console.log(keys)
+        
+    }
+
+
+    // give for of loop in array => to agr hm for in loop m array ko rpint krengy to ans me hmray pass counting
+// nhi yae gy blky array print hongy 
+
+
+    const val = ["Mrs Jihan" , "MS Haya"];
+for (const value of val) {
+//  console.log(value);
+    
+}
+
+//  ab keys ki value kse nikaly for in loop  m
+
+
+const programmings = [ 'css' , 'js' , 'html' , 'rb' , 'cpp']
+for (const keys in programmings) {
+   console.log(programming[keys])
+        
+    }
+
+    //  priorrities => arry p loop lgna hoto for of ,, or objectf p lgnahoto for in 
