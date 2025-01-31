4398e7f94b7054078c4100b285c4ccc734cc5dff
aptechhani <aptechhani@gmail.com>
2025-01-31 20:03:25 +0000

    loop2 confihure

---
+++ 04_basics/04_loopspart2.js
@@ -0,0 +1,56 @@
+// for of object means kis chez p loopp lgna h 
+
+const i = [ 2,4,5,6,7,8]
+for (const j of i) {
+
+//  console.log(j);
+    
+}
+const val = "Mrs Jihan"
+for (const value of val) {
+//  console.log(value);
+    
+}
+
+
+// maps m koi duplicate value nhi hoti 
+
+const map = new Map()
+map.set('PAK' , 'pakistan')
+map.set('USA' , 'uninted states of america')
+map.set('Fr' , 'France')
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key);
+//     console.log(value);
+// }
+
+// ,,,,,,,,,,,
+// for (const keys in map) {
+//     console.log(keys)
+// }
+
+//object p key value nhi lgenga maps p lg sakta h bbus
+// const myobj = {
+//     'g1' :'surwaysyrf' ,
+//      'g2' : 'templerun'
+//     }
+//     for (const  [key, value]  of myobj) {
+       
+//             // console.log(key);
+//             // console.log(value);
+//     }
+
+
+    // give object and print key value pair in loops
+
+    const myobjj = {
+        'g1' :'surwaysyrf' ,
+         'g2' : 'templerun' ,
+        }
+        for (const value in myobjj) {
+            // console.log(myobjj[value]);
+        }
+
