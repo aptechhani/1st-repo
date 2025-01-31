50ab0f163d8d8f6489bb08dd957240af61d4dea0
aptechhani <aptechhani@gmail.com>
2025-01-31 20:02:54 +0000

    loops 1configure

---
+++ 04_basics/03_loops.js
@@ -0,0 +1,24 @@
+let index = 0
+
+while (index <=10) {
+//  console.log(`value of index ${index}`);
+    index = index + 2
+}
+
+
+let myarray = ["batman", " superman " , " ironman" ]
+let arr =0
+while (arr < myarray.length) {
+    // console.log(`value is ${myarray[arr]}`);
+    arr++
+}
+
+
+// do while loop kam phly hota h condition bad m check hoti h 
+let score = 11
+do {
+// console.log(`score is ${score}`);
+score++
+
+    
+} while (score<=10);
